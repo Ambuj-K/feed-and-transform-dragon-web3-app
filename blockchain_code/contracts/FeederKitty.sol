76cc9a19250a1ba0bdd4bf43e7a1bd19463babb0
Ambuj-K <43566170+Ambuj-K@users.noreply.github.com>
2022-07-12 14:48:11 +0530

    Create FeederKitty.sol

---
+++ blockchain_code/contracts/FeederKitty.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract FeederFeeding is FeederFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  // Modify function definition here:
+  function feedAndMultiply(uint _feederId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_feederId]);
+    Feeder storage myFeeder = zombies[_feederId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myFeeder.dna + _targetDna) / 2;
+    // Add an if statement here
+    _createFeeder("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    // And modify function call here:
+    feedAndMultiply(_feederId, kittyDna);
+  }
+
+}
