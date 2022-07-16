2d95b8a6a0e72a94c3d49e7527873dd2e1faaa13
Ambuj-K <43566170+Ambuj-K@users.noreply.github.com>
2022-07-16 19:13:35 +0530

    Update DragonHelper.sol
    adding payable

--- blockchain_code/contracts/DragonHelper.sol
+++ blockchain_code/contracts/DragonHelper.sol
@@ -4,9 +4,25 @@ import "./DragonFeeding.sol";
 
 contract DragonHelper is DragonFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _dragonId) {
     require(dragons[_dragonId].level >= _level);
     _;
+  }
+
+  function withdraw() external onlyOwner {
+    address payable _owner = address(uint160(owner()));
+    _owner.transfer(address(this).balance);
+  }
+
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
+  function levelUp(uint _dragonId) external payable {
+    require(msg.value == levelUpFee);
+    dragons[_dragonId].level++;
   }
 
   function changeName(uint _dragonId, string calldata _newName) external aboveLevel(2, _dragonId) {
